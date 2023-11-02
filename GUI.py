139c6bec2000b31dc2d0e71ec2dd987634e34f60
jpetrasek1217 <josephpetrasek1017@gmail.com>
2023-11-02 17:00:19 -0400

    2.0.1: finished first revision of GUI code, but cannot test due to error in user_manager

--- GUI.py
+++ GUI.py
@@ -7,6 +7,12 @@ _VERSION_NUMBER = "1.0.7"
 _ICON_BITMAP_FILEPATH = "assets/pacemaker_icon.ico"
 _WINDOW_TITLE = "Pacemaker Device Controller Monitor " + _VERSION_NUMBER
 _PAD_X = 10
+_PAD_Y = 5
+_BUTTON_WIDTH = 8
+_BUTTON_BG = "white"
+_ENTRY_WIDTH = 20
+_ENTRY_BG = "white"
+_ENTRY_FG = "black"
 
 root = tk.Tk()
 root.title(_WINDOW_TITLE)
@@ -25,47 +31,85 @@ upperDCMFrame = tk.Frame(DCMFrame)
 parameterEntryAndLabelList = []
 
 
+def Start():
+    createAndShowWelcome()
+    return
+
 def createAndShowWelcome():   
     welcomeLabel = tk.Label(welcomeFrame, text="Welcome!",  font=("Montserrat", 16, "bold"))
     promptLabel = tk.Label(welcomeFrame, text="Please login or create a new user")
     usernameLabel = tk.Label(welcomeFrame, text="Username")
-    usernameInput = tk.Entry(welcomeFrame, width=20, bg="white", fg="black")
+    global usernameInput 
+    usernameInput = tk.Entry(welcomeFrame, width=_ENTRY_WIDTH, bg=_ENTRY_BG, fg=_ENTRY_FG)
     passwordLabel = tk.Label(welcomeFrame, text="Password")
-    passwordInput = tk.Entry(welcomeFrame, width=20, bg="white", fg="black", show="*")
-    loginButton = tk.Button(welcomeFrame, text="Login", padx=_PAD_X, pady=5, bg="white", command=on_Login)
-    createUserButton = tk.Button(welcomeFrame, text="Create New User", padx=10, pady=5, bg="white", command= on_registerUser) 
-
+    global passwordInput 
+    passwordInput = tk.Entry(welcomeFrame, width=_ENTRY_WIDTH, bg=_ENTRY_BG, fg=_ENTRY_FG, show="*")
+    loginButton = tk.Button(welcomeFrame, text="Login", padx=_PAD_X, pady=_PAD_Y, bg=_BUTTON_BG, command=onLogin)
+    createUserButton = tk.Button(welcomeFrame, text="Create New User", padx=_PAD_X, pady=_PAD_Y, bg=_BUTTON_BG, command= onRegisterUser) 
     welcomeFrame.grid(row=0, column=0, pady=25, padx=25, sticky="nsew")
-    welcomeLabel.grid(pady=10, row=0,column=0, columnspan=2)
-    promptLabel.grid(row=1, column=0, columnspan=2, pady=10)
-    usernameLabel.grid(pady=10,row=2, column=0, columnspan=2)
-    usernameInput.grid(row=3, column=0, padx=10, pady=10, columnspan=2)
-    passwordLabel.grid(pady=10,row=4, column=0, columnspan=2)
-    passwordInput.grid(row=5, column=0, padx=10, pady=20, columnspan=2)
-    loginButton.grid(row=6, column=0, padx=10, pady=10)
-    createUserButton.grid(row=6, column=1, padx=10, pady=10)
+    welcomeLabel.grid(pady=_PAD_Y, row=0,column=0, columnspan=2)
+    promptLabel.grid(row=1, column=0, columnspan=2, pady=_PAD_Y)
+    usernameLabel.grid(pady=_PAD_Y,row=2, column=0, columnspan=2)
+    usernameInput.grid(row=3, column=0, padx=_PAD_X, pady=_PAD_Y, columnspan=2)
+    passwordLabel.grid(pady=_PAD_Y,row=4, column=0, columnspan=2)
+    passwordInput.grid(row=5, column=0, padx=_PAD_X, pady=_PAD_Y, columnspan=2)
+    loginButton.grid(row=6, column=0, padx=_PAD_X, pady=_PAD_Y)
+    createUserButton.grid(row=6, column=1, padx=_PAD_X, pady=_PAD_Y)
+
+def onLogin():
+    isSuccessfulLogin, errorMsg = user_manager.loginUser(usernameInput.get(), passwordInput.get())
+    if not isSuccessfulLogin:
+         GUI_helpers.throwErrorPopup(errorMsg)
+         return
+    for widget in welcomeFrame.winfo_children():
+        widget.grid_forget()
+    welcomeFrame.grid_forget()
+
+    DCMFrame.grid(row=0, column=0, pady=25, padx=25, sticky="nsew")
+    upperDCMFrame.grid(row=0,column=0)
+    middleDCMFrame.grid(row=1,column=0)
+    lowerDCMFrame.grid(row=2,column=0)
+
+
+    createAndShowDCM(user_manager.getPacingMode())
 
 def createAndShowDCM(mode):
-    saveButton = tk.Button(lowerDCMFrame, text="Save", width=10, height=4, bg="white", command=onSaveParameters)
     verNumLabel = tk.Label(upperDCMFrame, text="Version " + _VERSION_NUMBER)
-    logoutButton = tk.Button(upperDCMFrame, text="Logout", bg="white", command=Logout)
-    institutionLabel = tk.Label(lowerDCMFrame, text="McMaster University")
+    logoutButton = tk.Button(upperDCMFrame, width=_BUTTON_WIDTH, text="Logout", bg="white", command=Logout)
 
+    updateParameters(mode)
+
+    global pacingModeButtonList
+    pacingModeButtonList = []
     for pacingMode in user_manager.getAllPacingModes():
-        pacingModeButton = tk.Button(lowerDCMFrame, text=pacingMode, padx=_PAD_X, pady=_PAD_Y, width=_BUTTON_WIDTH, bg=_BUTTON_BG, command= lambda: Change_Parameters(pacingMode))
-
-    sendButton =  tk.Button(lowerDCMFrame, text="Send", padx=10, pady=5, width=8, height=1, bg="white", command= lambda: EgramSend.grid(row=0, column=0, pady=25, padx=25, sticky="nsew"))
+        pacingModeButton = tk.Button(lowerDCMFrame, text=pacingMode, padx=_PAD_X, pady=_PAD_Y, width=_BUTTON_WIDTH, bg=_BUTTON_BG, command= lambda: updateParameters(pacingMode))
+        pacingModeButtonList.append(pacingModeButton)
+    currentPacingModeButton = 0
+    for ro in range(2):
+        for col in range(4):
+            pacingModeButtonList[currentPacingModeButton].grid(row=ro, column=col)
+            currentPacingModeButton += 1
+    saveButton = tk.Button(lowerDCMFrame, text="Save", width=_BUTTON_WIDTH, height=4, bg="white", command=onSaveParameters)
+    sendButton =  tk.Button(lowerDCMFrame, text="Send", padx=_PAD_X, pady=_PAD_Y, width=_BUTTON_WIDTH, height=1, bg="white", command= OpenEgram())
+    institutionLabel = tk.Label(lowerDCMFrame, text="McMaster University")
     
     DCMFrame.grid(row=0, column=0, pady=25, padx=25, sticky="nsew")
     upperDCMFrame.grid(row=0,column=0)
     middleDCMFrame.grid(row=1,column=0)
-    lowerDCMFrame.grid(row=1,column=0)
-    logoutButton.grid(row=0,column=4, columnspan=1, padx=10, pady=10)
+    lowerDCMFrame.grid(row=2,column=0)
+
     verNumLabel.grid(row=0, column=2)
+    logoutButton.grid(row=0,column=4, columnspan=1, padx=_PAD_X, pady=_PAD_Y)
+    
+    saveButton.grid(row=0, column=4, rowspan=2)
+    sendButton.grid(row=3, column=0)
+    institutionLabel.grid(row=2, column=1, columnspan=3, pady=_PAD_Y)
 
 
             
 def updateParameters(mode):
+    user_manager.savePacingMode(mode)
+    hideFrame(middleDCMFrame)
     Title_Label = tk.Label(upperDCMFrame, text="Mode: " + mode, font=("Montserrat", 14, "bold"))
     Title_Label.grid(row=0,column=0, columnspan=5, padx=10, pady=10, sticky="w")
     parameterEntryAndLabelList = []
@@ -75,43 +119,37 @@ def updateParameters(mode):
         paramValue = parameter[2]
         paramVisibility = parameter[3]
 
-        entry = tk.Entry(upperDCMFrame, width=10, bg="white", fg="black")
+        entry = tk.Entry(middleDCMFrame, width=10, bg="white", fg="black")
         entry.insert(0, paramValue)
         if not paramVisibility:
             entry.config(state="disabled")
 
-        label = tk.Label(upperDCMFrame, text=paramTitle)
+        label = tk.Label(middleDCMFrame, text=paramTitle)
       
         parameterEntryAndLabelList.append([paramName, entry, label])
 
-        for row in range(2):
-            for col in range(5):
-                index = row * 5 + col
-                if index >= len(parameterEntryAndLabelList):
-                    break
-                parameterEntryAndLabelList[index][1].grid(row = 2*row + 1, column=col, padx=10, pady=(15,5))
-                parameterEntryAndLabelList[index][2].grid(row = 2*row + 2, column=col, padx=10, pady=(5,15))
-            else:
-                continue    # only executed if the inner loop did NOT break
-            break           # only executed if the inner loop DID break
+    for row in range(2):
+        for col in range(5):
+            index = row * 5 + col
+            if index >= len(parameterEntryAndLabelList):
+                break
+            parameterEntryAndLabelList[index][1].grid(row = 2*row + 1, column=col, padx=10, pady=(15,5))
+            parameterEntryAndLabelList[index][2].grid(row = 2*row + 2, column=col, padx=10, pady=(5,15))
+        else:
+            continue    # only executed if the inner loop did NOT break
+        break           # only executed if the inner loop DID break
 
 
 def Logout():
     user_manager.logoutUser()
     hideFrame(DCMFrame)
-    showWelcome()
+    createAndShowWelcome()
     return
     
 def hideFrame(frame):
     for widget in frame.winfo_children():
         widget.grid_forget()
     frame.grid_forget()
-
-def Change_Parameters(mode):
-    user_manager.savePacingMode(mode)
-    for widget in upperDCMFrame.winfo_children():
-        widget.grid_forget()
-    createAllDCMItems(mode)
 
 def onSaveParameters():
     for paramEntryAndLabel in parameterEntryAndLabelList:
@@ -124,44 +162,7 @@ def onSaveParameters():
             return
     GUI_helpers.throwSuccessPopup("Successfully saved all parameters.")
 
-
-
-def on_Login():
-    isSuccessfulLogin, errorMsg = user_manager.loginUser(Username_input.get(), Password_input.get())
-    if not isSuccessfulLogin:
-         GUI_helpers.throwErrorPopup(errorMsg)
-         return
-    for widget in welcomeFrame.winfo_children():
-        widget.grid_forget()
-    welcomeFrame.grid_forget()
-
-    DCMFrame.grid(row=0, column=0, pady=25, padx=25, sticky="nsew")
-    upperDCMFrame.grid(row=0,column=0)
-    lowerDCMFrame.grid(row=1,column=0)
-
-    saveButton.grid(row=0, column=4, rowspan=2)
-    verNumLabel.grid(row=0, column=2)
-    logoutButton.grid(row=0,column=4, columnspan=1, padx=10, pady=10)
-
-    AOOButton.grid(row=1, column=0)
-    AAIButton.grid(row=0, column=0)
-    VOOButton.grid(row=1, column=1)
-    VVIButton.grid(row=0, column=1)
-    AOORButton.grid(row=1, column=2)
-    AAIRButton.grid(row=0, column=2)
-    VOORButton.grid(row=1, column=3)
-    VVIRButton.grid(row=0, column=3)
-    sendButton.grid(row=2, column=0)
-    institutionLabel.grid(row=2, column=1, columnspan=3, pady=(10,0))
-
-    createAllDCMItems(user_manager.getPacingMode())
-
-
-def Start():
-    showWelcome()
-    return
-
-def on_registerUser():
+def onRegisterUser():
     isSuccessfulRegister, errorMsg = user_manager.registerUser(usernameInput.get(), passwordInput.get())
     if isSuccessfulRegister:
          GUI_helpers.throwSuccessPopup(f"Successfully Created New User \'{usernameInput.get().strip()}\'")
@@ -174,15 +175,6 @@ def OpenEgram():
     for widget in welcomeFrame.winfo_children():
         widget.grid_forget()
     welcomeFrame.grid_forget()
-    
-    
-
-
-# Welcome Frame
-
-
-
-# DCM Frame
 
 
 Start()
